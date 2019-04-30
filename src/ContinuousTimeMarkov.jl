2647d8041abc3f9d0f188016919329a29a380810
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-30 23:36:13 +0000

    Snapshot of branch master taken on 2019-04-30T19:36:12.76-04:00 from "https://github.com/UnofficialJuliaMirror/ContinuousTimeMarkov.jl-eab40c8d-01e0-5a99-916a-641eeda4da29"

--- src/ContinuousTimeMarkov.jl
+++ src/ContinuousTimeMarkov.jl
@@ -4,11 +4,11 @@ export TransitionRateMatrix, TransitionRateMatrix!, stationary_distribution
 
 using AlgebraResultTypes: result_ring
 using ArgCheck: @argcheck
-using Distributions: Categorical, Exponential
+using Distributions: Categorical, Exponential, probs
 using DocStringExtensions: SIGNATURES
 using Parameters: @unpack
 using LinearAlgebra: Diagonal, lu, normalize!
-using Random: AbstractRNG
+using Random: AbstractRNG, SamplerTrivial
 using SparseArrays: rowvals, nonzeros, nzrange, SparseMatrixCSC
 
 include("transition_matrices.jl")
